03030939f12f3b1a66cb55cf885cd61e07f8da7d
kaedeed <akshitsrivastava.edu@gmail.com>
2025-02-13 22:13:37 +0530

    Update LoveLetter.js

--- src/components/LoveLetter/LoveLetter.js
+++ src/components/LoveLetter/LoveLetter.js
@@ -35,10 +35,55 @@ const LoveLetter = () => {
       <div className="flap"></div>
       <div className="body"></div>
       <div className={`letter ${isFullSize ? 'fullSize' : ''}`}>
-        mah dear kundanapu bomma🖤,<br />
-        never have i been so blessed as to fall in love with someone as wonderful as you...<br />i loveeee youuuu :)<br />
-        always yours,<br />
-        @niihaaarrrr
+        import React, { useState, useRef } from 'react';
+import './LoveLetter.css';
+import audioFile from './kushi.mp3';
+
+const LoveLetter = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isFullSize, setIsFullSize] = useState(false);
+  const audioRef = useRef(null);
+
+  const handleOpenLetter = () => {
+    setIsOpen(true);
+    setTimeout(() => {
+      setIsFullSize(true);
+      // Ensuring audio play is directly a result of this user interaction
+      if (audioRef.current) {
+        audioRef.current.play()
+          .then(() => console.log("Playback succeeded"))
+          .catch(e => console.error("Playback failed:", e));
+      }
+    }, 800);
+  };
+
+  const handleCloseLetter = () => {
+    setIsFullSize(false);
+    setTimeout(() => {
+      if (audioRef.current) {
+        audioRef.current.pause();
+      }
+      setIsOpen(false);
+    }, 800);
+  };
+
+  return (
+    <div className={`envelope ${isOpen ? 'open' : ''}`} onClick={!isFullSize ? handleOpenLetter : handleCloseLetter}>
+      <div className="flap"></div>
+      <div className="body"></div>
+      <div className={`letter ${isFullSize ? 'fullSize' : ''}`}>
+        Dear Cupcake 🖤,<br />
+        I cannot express how much i love you, you have always been there for me and i want you to know i am there for you in the every step of your life as well, you dont know how grateful i am to god that i have got a person like you in my life.<br />Chaand Sitaaro ko asmaan me Dhoondhte hai sabhi<br />
+        Par hote hai kuch is zameen par bhi<br />Jhalaktein Aakhon mein jaise ho dhoop savere ki<br />Zindagi mein faila de khoobsurti aur khushiyan<br />Jaise Hawaye saanjh ki...<br />Sukoon us awaaaz me jaise dawayein dard ki<br />Ab aage kya bolu....Samandar hai wahi mere in duaon ki<br />
+        @Yours Aki
+      </div>
+      <audio ref={audioRef} src={audioFile} onError={(e) => console.error('Audio error:', e.message)} />
+    </div>
+  );
+};
+
+export default LoveLetter;
+
       </div>
       <audio ref={audioRef} src={audioFile} onError={(e) => console.error('Audio error:', e.message)} />
     </div>
